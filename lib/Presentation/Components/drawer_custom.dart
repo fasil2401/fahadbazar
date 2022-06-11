c85f3d045051d2cb331a9fded5f50a79b9091c68
fasil2401 <fasil2401@gmail.com>
2022-06-11 19:11:55 +0530

    drawer page initiate

--- lib/Presentation/Components/drawer_custom.dart
+++ lib/Presentation/Components/drawer_custom.dart
@@ -1,6 +1,10 @@
 
+import 'package:fahadbazar/Presentation/constants/border_radius.dart';
 import 'package:fahadbazar/Presentation/constants/colors.dart';
+import 'package:fahadbazar/Presentation/constants/heights.dart';
+import 'package:fahadbazar/Presentation/constants/paddings.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class DrawerCustom extends StatelessWidget {
   const DrawerCustom({
@@ -11,41 +15,52 @@ class DrawerCustom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
      backgroundColor: commonScaffoldBack,
-     // Add a ListView to the drawer. This ensures the user can scroll
-     // through the options in the drawer if there isn't enough vertical
-     // space to fit everything.
-     child: ListView(
-       // Important: Remove any padding from the ListView.
-       padding: EdgeInsets.zero,
-       children: [
-         ListTile(
-           title: const Text('Item 1'),
-           onTap: () {
-             // Update the state of the app
-             // ...
-             // Then close the drawer
-             Navigator.pop(context);
-           },
-         ),
-         ListTile(
-           title: const Text('Item 2'),
-           onTap: () {
-             // Update the state of the app
-             // ...
-             // Then close the drawer
-             Navigator.pop(context);
-           },
-         ),
-         ListTile(
-           title: const Text('Item 2'),
-           onTap: () {
-             // Update the state of the app
-             // ...
-             // Then close the drawer
-             Navigator.pop(context);
-           },
-         ),
-       ],
+    
+     child: Padding(
+       padding: EdgeInsets.symmetric(horizontal: 7.w),
+       child: Column(
+         children: [
+           commonHeight3,
+           Container(
+             height: 15.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: mutedBackColor,
+              borderRadius: cartRadius*1.6
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 9.w,
+                  backgroundImage:const AssetImage('assets/images/user.png'),
+                ),
+                SizedBox(width: 3.w,)
+              ],
+            ),
+           ),
+           ListTile(
+             title: const Text('Item 1'),
+             onTap: () {
+               Navigator.pop(context);
+             },
+           ),
+           ListTile(
+             title: const Text('Item 2'),
+             onTap: () {
+               Navigator.pop(context);
+             },
+           ),
+           ListTile(
+             title: const Text('Item 2'),
+             onTap: () {
+               // Update the state of the app
+               // ...
+               // Then close the drawer
+               Navigator.pop(context);
+             },
+           ),
+         ],
+       ),
      ),
       );
   }
